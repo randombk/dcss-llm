3bc35e916f8bdba551c353b151baa0dc983fec4c
David Li <jiawei.davidli@gmail.com>
2025-03-12 22:15:23 -0700

    Add a mechanism to patch DCSS to output game state every turn
    Signed-off-by: David Li <jiawei.davidli@gmail.com>

--- build-crawl.sh
+++ build-crawl.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 set -e
 
+# Reset and clean the repository
 cd crawl/crawl-ref/source
+git reset --hard
+
+# Apply all patch files
+cp -r ../../../crawl-patches/* .
+git apply *.llm-patch
+rm -rf *.llm-patch
+
+# Build the game
 make -j `nproc` CFLAGS="-DLLM_DATA_DUMP=1" 
+
+# Reset the repository to the upstream state to avoid polluting `git status`
+git add -A
+git reset --hard
